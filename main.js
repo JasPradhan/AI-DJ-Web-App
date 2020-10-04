fe9588464361c4aae5c99f50ad57e6a3695b3005
JasPradhan <70465360+JasPradhan@users.noreply.github.com>
2020-10-04 12:37:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 song="";
 
+leftWristX=0;
+leftWristY=0;
+
+rightWristX=0;
+rightWristY=0;
+
 function preload(){
 song=loadSound("music.mp3");
 }
@@ -9,6 +15,12 @@ canvas=createCanvas(600, 500);
 canvas.center();
 video=createCapture(VIDEO);
 video.hide();
+poseNet=ml5.poseNet(video,modeLoaded);
+poseNet.on('pose',gotPoses)
+}
+
+function modeLoaded(){
+    console.log("poseNet is initialized")
 }
 
 function draw(){
@@ -17,4 +29,22 @@ image(video, 0, 0, 600, 500)
 
 function play(){
     song.play();
+    song.setVolume(0.1);
+    song.rate(1);
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+
+        console.log("left wrist X = "+leftWristX+"left wrist Y = "+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+
+        console.log("right wrist X = "+rightWristX+"right wrist Y = "+rightWristY);
+
+    }
 }
