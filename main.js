e75cd1873945716d2dcee728be7fb7fac178fff5
JasPradhan <70465360+JasPradhan@users.noreply.github.com>
2020-10-11 12:43:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 score_leftWrist=0;
+score_rightWrist=0;
 
 song="";
 
@@ -32,6 +33,35 @@ fill("#3cff00");
 
 strokeWeight(2);
 stroke("#ff0000");
+if(score_rightWrist>0.2){
+    circle(rightWristX ,rightWristY ,20);
+
+    if(rightWristY>0 && rightWristY<100){
+        document.getElementById("speed").innerHTML="Speed: 0.5x";
+        song.rate(0.5);
+    }
+    
+    else if(rightWristY>100 && rightWristY<200){
+        document.getElementById("speed").innerHTML="Speed: 1x";
+        song.rate(1);
+    }
+    
+    else if(rightWristY>200 && rightWristY<300){
+        document.getElementById("speed").innerHTML="Speed: 1.5x";
+        song.rate(1.5);
+    }
+    
+    else if(rightWristY>300 && rightWristY<400){
+        document.getElementById("speed").innerHTML="Speed: 2x";
+        song.rate(2);
+    }
+    
+    else if(rightWristY>400 && rightWristY<500){
+        document.getElementById("speed").innerHTML="Speed: 2.5x";
+        song.rate(2.5);
+    }    
+}
+
 if(score_leftWrist>0.2){
     circle(leftWristX ,leftWristY ,20);
     inNumberleft=Number(leftWristY);
@@ -54,6 +84,9 @@ function gotPoses(results){
 
         score_leftWrist=results[0].pose.keypoints[9].score;
         console.log("Score Of Left Wrist= "+score_leftWrist);
+
+        score_rightWrist=results[0].pose.keypoints[10].score;
+        console.log("Score Of Right Wrist= "+score_rightWrist);
 
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
